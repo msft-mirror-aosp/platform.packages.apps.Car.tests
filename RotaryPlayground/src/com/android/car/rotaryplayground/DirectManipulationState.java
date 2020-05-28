97c16099017c074fc8292c17959966a5e4e26f61
Pardis Beikzadeh <pardis@google.com>
2020-05-28 16:52:08 -0700

    Reduce code duplication further.
    Given that onPause() method needs to also disable DM mode,
    I moved the logic around some more so it can be reused
    there and by the DirectManipulationHandler class.
    
    Also fixed a bug I found that didn't consume nudge and
    rotation events when no handler delegate was provided for
    them. Not consuming the nudge events means that in DM mode
    user can nudge out of the view that should be directly
    manipulated. The rotation one doesn't cause an issue
    as far as I can tell but I am consuming them because
    that is what the javadoc claims to do (provide a "noop"
    impl if a delegate not provided = consume event and
    do nothing).
    
    BUG: 156978213
    Test: make, install, run code
    Change-Id: I417b2dcbd5e0fd36e11db58681a8baff9bbfcf8e

--- RotaryPlayground/src/com/android/car/rotaryplayground/DirectManipulationState.java
+++ RotaryPlayground/src/com/android/car/rotaryplayground/DirectManipulationState.java
@@ -16,9 +16,14 @@
 
 package com.android.car.rotaryplayground;
 
+import android.graphics.Color;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.android.car.ui.utils.DirectManipulationHelper;
 
 /**
  * Keeps track of the state of "direct manipulation" Rotary mode for this application window by
@@ -29,19 +34,18 @@ import androidx.annotation.Nullable;
  */
 public class DirectManipulationState {
 
+
+    /** Background color of a view when it's in direct manipulation mode. */
+    private static final int BACKGROUND_COLOR_IN_DIRECT_MANIPULATION_MODE = Color.BLUE;
+
+    /** Background color of a view when it's not in direct manipulation mode. */
+    private static final int BACKGROUND_COLOR_NOT_IN_DIRECT_MANIPULATION_MODE = Color.TRANSPARENT;
+
     /** The view that is in direct manipulation mode, or null if none. */
     @Nullable private View mViewInDirectManipulationMode;
 
-    public void setStartingView(@Nullable View view) {
+    private void setStartingView(@Nullable View view) {
         mViewInDirectManipulationMode = view;
-    }
-
-    /**
-     * Returns the {@link View} from which we entered into Direct Manipulation mode when that mode
-     * is active, and null otherwise.
-     */
-    @Nullable public View getStartingView() {
-        return mViewInDirectManipulationMode;
     }
 
     /**
@@ -49,5 +53,46 @@ public class DirectManipulationState {
      */
     public boolean isActive() {
         return mViewInDirectManipulationMode != null;
+    }
+
+    /**
+     * Enables Direct Manipulation mode, and keeps track of {@code view} as the starting point
+     * of this transition.
+     * <p>
+     * We generally want to give some kind of visual indication that this change has happened. In
+     * this example we change the background color of {@code view}.
+     *
+     * @param view - the {@link View} from which we entered into Direct Manipulation mode.
+     */
+    public void enable(@NonNull View view) {
+        /*
+         * A more robust approach would be to fetch the current background color from
+         * the view object and store it back onto the View itself using the {@link
+         * View#setTag(int, java.lang.Object)} API. This could then be fetched back
+         * and used to restore the background color without needing to keep a constant
+         * reference to the color here which could fall out of sync with the xml files.
+         */
+        view.setBackgroundColor(BACKGROUND_COLOR_IN_DIRECT_MANIPULATION_MODE);
+        DirectManipulationHelper.enableDirectManipulationMode(view, /* enable= */ true);
+        setStartingView(view);
+    }
+
+    /**
+     * Disables Direct Manipulation mode and restores any visual indicators for the {@link View}
+     * from which we entered into Direct Manipulation mode.
+     */
+    public void disable() {
+        mViewInDirectManipulationMode.setBackgroundColor(
+                BACKGROUND_COLOR_NOT_IN_DIRECT_MANIPULATION_MODE);
+        DirectManipulationHelper.enableDirectManipulationMode(
+                mViewInDirectManipulationMode, /* enable= */ false);
+        // For ViewGroup objects, restore descendant focusability to FOCUS_BLOCK_DESCENDANTS so
+        // during non-Direct Manipulation mode, aka, general rotary navigation, we don't go
+        // through the individual inner UI elements.
+        if (mViewInDirectManipulationMode instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) mViewInDirectManipulationMode;
+            viewGroup.setDescendantFocusability(ViewGroup.FOCUS_BLOCK_DESCENDANTS);
+        }
+        setStartingView(null);
     }
 }
