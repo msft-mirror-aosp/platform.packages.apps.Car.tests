b93e78a05775a09c6a26396b5156b77556a3f053
Arnaud Berry <arnaudberry@google.com>
2023-08-28 10:57:47 -0700

    Update immersive logos, remove non tintable ones
    Bug: 292112087
    Test: manual
    Change-Id: I61e4e37d2d70fb55ecc418e6a2a20e5bb8d20247

--- TestMediaApp/common/src/com/android/car/media/testmediaapp/MediaConstants.java
+++ TestMediaApp/common/src/com/android/car/media/testmediaapp/MediaConstants.java
@@ -33,12 +33,4 @@ public class MediaConstants {
             "androidx.car.app.mediaextensions.KEY_CONTENT_FORMAT_TINTABLE_LARGE_ICON_URI";
     public static final String KEY_CONTENT_FORMAT_TINTABLE_SMALL_ICON_URI =
             "androidx.car.app.mediaextensions.KEY_CONTENT_FORMAT_TINTABLE_SMALL_ICON_URI";
-    public static final String KEY_CONTENT_FORMAT_DARK_MODE_LARGE_ICON_URI =
-            "androidx.car.app.mediaextensions.KEY_CONTENT_FORMAT_DARK_MODE_LARGE_ICON_URI";
-    public static final String KEY_CONTENT_FORMAT_LIGHT_MODE_LARGE_ICON_URI =
-            "androidx.car.app.mediaextensions.KEY_CONTENT_FORMAT_LIGHT_MODE_LARGE_ICON_URI";
-    public static final String KEY_CONTENT_FORMAT_DARK_MODE_SMALL_ICON_URI =
-            "androidx.car.app.mediaextensions.KEY_CONTENT_FORMAT_DARK_MODE_SMALL_ICON_URI";
-    public static final String KEY_CONTENT_FORMAT_LIGHT_MODE_SMALL_ICON_URI =
-            "androidx.car.app.mediaextensions.KEY_CONTENT_FORMAT_LIGHT_MODE_SMALL_ICON_URI";
 }
